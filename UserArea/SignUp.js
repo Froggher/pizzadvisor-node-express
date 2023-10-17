28a01b62e90c2da502bfa0d0b63b6a9f569952e4
Froggher <federicoranocchia@gmail.com>
2023-10-17 05:02:16 +0200

    ♻️ Ricreato SignUp con le function async 🧑‍💻 Il file Fun.js serve per contenere funzioni che verranno utilizzate piú volte nelle route

--- UserArea/SignUp.js
+++ UserArea/SignUp.js
@@ -1,33 +1,42 @@
 import { pool } from "../config.js";
+import { DatabaseConnection } from "../misc/Fun.js";
 
 
 export async function SignUp(req, res, next) {
-    const { email, psw, first_name, last_name } = req.body
-    console.log(psw)
-    console.log(req.body)
+
+    // Conn deve stare fuori per chiudere la connessione
     let conn;
+
     try {
-        conn = await pool.getConnection();
-        //const rows = await conn.query('SELECT Column1 FROM Esempio.datibellissimi;');
-        //console.log(rows); //[ {val: 1}, meta: ... ]
-        const results = await conn.query("INSERT INTO `User`.`User` value (?,?,?,?)", [email, psw, first_name, last_name]);
-        //console.log(res)
-        //console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-        console.log(results)
-        res.status(200).send({
-            message: 'Account creato con successo'
-        });
-    } catch (err) {
-        console.error('Errore nella connessione al database:', err);
-        res.status(500).send({ message: 'Errore creazione account', });
+        conn = await DatabaseConnection(res, pool);
+        await SignUpPost(req, res, conn)
+    } catch (error) {
+        console.log('Qualcosa é andato con la creazione account')
     } finally {
         if (conn) return conn.end();
     }
+
+
+
 }
 
+// Si occupa di immettere i dati utente in una tabella del db
+async function SignUpPost(req, res, conn) {
+    const { email, psw, first_name, last_name } = req.body
+    try {
+        await conn.query("INSERT INTO `User`.`User` value (?,?,?,?)", [email, psw, first_name, last_name]);
+        //console.log(results)
+        res.status(201).send({
+            message: 'Account creato con successo'
+        });
+    } catch (err) {
+        console.error('Errore creazione account:', err.errno);
+        // Se l'errore riportato dal database corrisponde a 1062 significa che c'é un valore duplicato per la email
+        if (err.errno === 1062) {
+            res.status(409).send({ message: 'Utente giá registrato', });
+        }
+        res.status(400).send({ message: 'Errore nella creazione account', });
+    }
+}
 
-// const TestDatabase = () => {
-// pool.
-
-// }
 
---
+++ misc/Fun.js
@@ -0,0 +1,11 @@
+// Verifica che il collegamento con il database viene effettuato correttamente
+export async function DatabaseConnection(res, pool) {
+    try {
+        const conn = await pool.getConnection();
+        console.log(conn)
+        return conn;
+    } catch (err) {
+        console.error('Errore nella connessione al database:', err);
+        res.status(500).send({ message: 'Il server non é riuscito a collegarsi al database', });
+    }
+}
